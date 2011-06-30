7089dbf725ddcf16a3ef2012294704de794e45fa
Marco Aurélio <thecoreh@gmail.com>
2011-06-30 15:29:26 -0300

    Adicionada funcionalidade de Usuarios

---
+++ app/controllers/UsuarioController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class UsuarioController extends Controller {
+
+    public static void listar() {
+        List usuarios = Usuario.find("order by nome asc").fetch();
+        render(usuarios);
+    }
+    
+    public static void criar(String nome, String senha) {
+        Usuario usuario = new Usuario(nome, senha);
+        usuario.save();
+        String mensagem = "Usuário Criado Com Sucesso";
+        render(mensagem);
+    }
+
+}
