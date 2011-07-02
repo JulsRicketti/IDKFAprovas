3b4f9286b1a86fcd634f7cbd5837f680e9b5ef50
Julia <julsricketti@gmail.com>
2011-07-02 09:56:06 -0300

    Classe Questao implementada

--- app/controllers/QuestaoController.java
+++ app/controllers/QuestaoController.java
@@ -7,26 +7,26 @@ import java.util.*;
 
 import models.*;
 
-@With(AdministradorArea.class)
+@With(ProfessorArea.class)
 public class QuestaoController extends GenericController {
 
     public static void listar() {
-        List questaoes = Questao.find("order by nome asc").fetch();
-        render(questaoes);
+        List questoes = Questao.find("order by codigo asc").fetch();
+        render(questoes);
     }
     
     public static void criar(String codigo, String enunciado, String tipo, String referenciaBibliografica) {
         Questao questao = new Questao(codigo, enunciado, tipo, referenciaBibliografica);
         questao.save();
-        String mensagem = "Questao Criado Com Sucesso";
+        String mensagem = "Questao Criada Com Sucesso";
         render(mensagem);
     }
     
-    public static void deletar(String matricula)
+    public static void deletar(String codigo)
     {
-    	List <Questao> questaoes = Questao.find("matricula = ?", matricula).fetch();
-    	for (Questao p : questaoes){
-    		p.delete();
+    	List <Questao> questoes = Questao.find("codigo = ?", codigo).fetch();
+    	for (Questao q : questoes){
+    		q.delete();
     	}
     	redirect("QuestaoController.listar");
     }
